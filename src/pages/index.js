0755891f537073db3be1796a7746016582691476
fahimgithub1 <fahimjoy768@gmail.com>
2023-03-22 00:05:29 +0600

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Header from "@/components/header";
+import Navigation from "@/navigation-footer/navigation";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Navigation />
+        {/* <h1 className="text-4xl font-bold underline">Hello world!</h1> */}
+      </main>
+    </>
+  );
+}
